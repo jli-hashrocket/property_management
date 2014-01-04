b2276ef2bf7751b18deb871f535443fb43e6e7c2
macheteshuffle <jeff.li029@gmail.com>
2014-01-04 15:46:34 -0500

    created edit and delete building feature, passed tests

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -20,6 +20,26 @@ class BuildingsController < ApplicationController
     @building = Building.find(params[:id])
   end
 
+  def edit
+    @building = Building.find(params[:id])
+  end
+
+  def update
+    @building = Building.find(params[:id])
+    if @building.update
+      redirect_to @building, notice: "Building was updated successfully"
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @building = Building.find(params[:id])
+    if @building.destroy
+      redirect_to buildings_url, notice: "Building was successfully deleted"
+    end
+  end
+
   private
     def building_params
       params.require(:building).permit(:address, :city, :state, :zip, :description)
